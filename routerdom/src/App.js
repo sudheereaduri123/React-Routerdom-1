4879b651700a58a2dfaca2142d85f35e4b871777
sudheereaduri123 <eadurisudheer@gmail.com>
2024-12-04 11:26:40 +0530

    Add files via upload

---
+++ routerdom/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './app.css';
+
+function APP(){
+  return{
+    <div></div>
+
+  }
+}
---
+++ routerdom/src/components/singup.js
@@ -0,0 +1,41 @@
+import react from 'react'
+function signup() {
+    return(
+        <div>
+            <form>
+                <h2>signup</h2>
+                <div>
+                    <label>firstName</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>lastName</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>age</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>mobile No</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>profile Pic</label>
+                    <input type="file"></input>
+                </div>
+                <button>signup</button>
+            </form>
+            </div>
+            )
+            };
+
+export default "APP";
